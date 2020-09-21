e4e8280b3717a361e930fb51e8800dd538585e1f
Randy_Sw <srandy1998@outlook.com>
2020-09-21 15:26:13 +0800

    update

--- raw_EMG_ConvNet/domain_adversarial_network.py
+++ raw_EMG_ConvNet/domain_adversarial_network.py
@@ -8,7 +8,89 @@ from torch.autograd import Function
 import torchvision.transforms as transforms
 
 
+class ReverseLayerF(Function):
+
+    @staticmethod
+    def forward(ctx, x, alpha):
+        ctx.alpha = alpha
+
+        return x.view_as(x)
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        output = grad_output.neg() * ctx.alpha
+
+        return output, None
+
+
 # %% DaNN
+class DaNN_net(nn.Module):
+    def __init__(self, number_of_classes,):
+        super(DaNN_net, self).__init__()
+        self.feature = nn.Sequential(
+            nn.Conv2d(1, 32, kernel_size=(3, 5)),
+            nn.BatchNorm2d(32),
+            nn.PReLU(32),
+            nn.Dropout2d(0.5),
+
+            nn.MaxPool2d(kernel_size=(1, 3)),
+
+            nn.Conv2d(32, 64, kernel_size=(3, 5)),
+            nn.BatchNorm2d(64),
+            nn.PReLU(64),
+            nn.Dropout2d(0.5),
+
+            nn.MaxPool2d(kernel_size=(1, 3)),
+        )
+
+        self.domain_classifier = nn.Sequential(
+            nn.Linear(1024, 512),
+            nn.BatchNorm1d(512),
+            nn.PReLU(),
+
+            nn.Linear(512, 512),
+            nn.BatchNorm1d(512),
+            nn.PReLU(),
+
+            nn.Linear(512, 512),
+            nn.BatchNorm1d(512),
+            nn.PReLU(),
+
+            nn.Linear(512, 512),
+            nn.BatchNorm1d(512),
+            nn.PReLU(),
+
+            nn.Linear(512, 1),
+        )
+
+        self.label_classifier = nn.Sequential(
+            nn.Linear(1024, 512),
+            nn.BatchNorm1d(512),
+            nn.PReLU(512),
+            nn.Dropout(0.5),
+
+            nn.Linear(512, number_of_classes)
+        )
+
+        self.initialize_weights()
+
+    def forward(self, input_data, alpha):
+        feature = self.feature(input_data)
+        feature = feature.view(-1, 1024)
+        reverse_feature = ReverseLayerF.apply(feature, alpha)
+        class_output = self.label_classifier(feature)
+        domain_output = self.domain_classifier(feature)
+
+    def initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                torch.nn.init.kaiming_normal_(m.weight)
+                m.bias.data.zero_()
+            elif isinstance(m, nn.Linear):
+                torch.nn.init.kaiming_normal_(m.weight)
+                m.bias.data.zero_()
+
+
 class FeatureExtractor(nn.Module):
 
     def __init__(self, number_of_classes, ):
@@ -32,8 +114,8 @@ class FeatureExtractor(nn.Module):
         self.initialize_weights()
 
     def forward(self, x):
-        x = self.conv(x).squeeze()
-        return x
+        feature_vector = self.conv(x).squeeze()
+        return feature_vector
 
     def initialize_weights(self):
         for m in self.modules():
@@ -101,8 +183,8 @@ class LabelPredictor(nn.Module):
     def forward(self, feature_vector):
         output = self.layer(feature_vector)
         flatten_tensor = output.view(-1, 1024)
-        label = self.fc(flatten_tensor)
-        return label
+        class_label = self.fc(flatten_tensor)
+        return class_label
 
     def initialize_weights(self):
         for m in self.modules():
@@ -116,13 +198,16 @@ class LabelPredictor(nn.Module):
 
 if __name__ == '__main__':
     # init network components
-    feature_extractor = FeatureExtractor(number_of_classes=10).cuda()
-    label_predictor = LabelPredictor(number_of_classes=10).cuda()
-    domain_classifier = DomainClassifier().cuda()
+    # feature_extractor = FeatureExtractor(number_of_classes=10).cuda()
+    # label_predictor = LabelPredictor(number_of_classes=10).cuda()
+    # domain_classifier = DomainClassifier().cuda()
+    #
+    # print(feature_extractor)
+    # print(label_predictor)
+    # print(domain_classifier)
 
-    print(feature_extractor)
-    print(label_predictor)
-    print(domain_classifier)
+    DaNN_net = DaNN_net(number_of_classes=10).cuda()
+    print(DaNN_net)
 
 
 
