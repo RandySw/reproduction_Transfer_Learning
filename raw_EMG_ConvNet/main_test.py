238f17dbfe23eef13909d36d1e000bcdba7711e8
Randy_Sw <srandy1998@outlook.com>
2020-09-26 22:41:49 +0800

    update

--- raw_EMG_ConvNet/main_test.py
+++ raw_EMG_ConvNet/main_test.py
@@ -8,8 +8,15 @@ from torch.utils.data import TensorDataset, DataLoader
 from torch.autograd import Variable
 
 import time
+import os
+import copy
 
 from domain_adversarial_network import DaNNet, FeatureExtractor, DomainClassifier, LabelPredictor
+
+
+torch.cuda.set_device(0)
+seed = 0
+torch.manual_seed(seed)
 
 
 def scramble(examples, labels):
@@ -162,8 +169,9 @@ print('Loading target training data finished.\n\n')
 
 # %% Network initialization
 # net = DaNNet(number_of_classes=7)
-best_loss = float('inf')
-num_epochs = 10
+best_loss_F = float('inf')
+
+num_epochs = 100
 criterion = nn.CrossEntropyLoss(reduction='sum')
 # optimizer = optim.Adam(net.parameters(), lr=0.001)
 precision = 1e-8
@@ -179,7 +187,13 @@ optimizer_F = optim.Adam(feature_extractor.parameters())
 optimizer_C = optim.Adam(label_predictor.parameters())
 optimizer_D = optim.Adam(domain_classifier.parameters())
 
-lamb = 1
+scheduler_F = optim.lr_scheduler.ReduceLROnPlateau(optimizer=optimizer_F, mode='min', factor=.2, patience=5,
+                                                 verbose=False, eps=precision)
+scheduler_D = optim.lr_scheduler.ReduceLROnPlateau(optimizer=optimizer_D, mode='min', factor=.2, patience=5,
+                                                 verbose=False, eps=precision)
+
+
+lamb = 0.1
 
 # %% Training stage
 source_train_dataloaders = {'train': list_source_train_dataloader, 'valid': list_source_valid_dataloader}
@@ -187,20 +201,17 @@ target_train_dataloaders = {'train': list_target_train_dataloader, 'valid': list
 
 # Traversal all target subjects
 for target_subject_train_loader, target_subject_valid_loader in zip(list_target_train_dataloader, list_target_valid_dataloader):
-    patience = 30
-    patience_increase = 30
-    # print(type(target_subject_train_loader))
-    # print(type(list_target_train_dataloader[0]))
-    # print(len(list_target_train_dataloader[0]))
-    # time.sleep(1000)
+    patience = 10
+    patience_increase = 10
 
-    target_subject_num = 0
+    best_weights_feature_extractor = copy.deepcopy(feature_extractor.state_dict())
+    best_weights_domain_classifier = copy.deepcopy(domain_classifier.state_dict())
+    best_weights_label_predictor = copy.deepcopy(label_predictor.state_dict())
 
     for epoch in range(num_epochs):
         epoch_start = time.time()
         print('-' * 20)
-        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
-
+        print('Epoch {}/{}'.format(epoch + 1, num_epochs))
 
         for phase in ['train', 'valid']:
             if phase == 'train':
@@ -212,14 +223,10 @@ for target_subject_train_loader, target_subject_valid_loader in zip(list_target_
                 domain_classifier.train(False)
                 label_predictor.train(False)
 
-            total_hit, total_num = 0.0, 0.0
-            source_total_hit, target_total_hit = 0.0, 0.0
-            # source_total_num, target_total_num = 0.0, 0.0
+            epoch_F_loss, epoch_D_loss = 0.0, 0.0
 
-
-            running_loss = 0.
-            running_corrects = 0
-            total = 0
+            epoch_source_hit, epoch_target_hit = 0.0, 0.0
+            epoch_source_num, epoch_target_num = 0.0, 0.0
 
             # get a random order for the source training subject
             random_vec = np.arange(len(source_train_dataloaders[phase]))  # len(random_vec) = 19
@@ -228,27 +235,24 @@ for target_subject_train_loader, target_subject_valid_loader in zip(list_target_
 
             # 遍历 source domain 的每个subject dataloader
             for dataset_index in random_vec:
-                loss_over_batches = 0.
-                batch_D_loss, batch_F_loss = 0.0, 0.0
+                num_batches = 0
 
-                correct_over_batches = 0.
-                correct_source_over_batches, correct_target_over_batches = 0., 0.
-
-                total_source_num, total_target_num = 0.0, 0.0
-
-                running_D_loss, running_F_loss = 0.0, 0.0
-                running_source_corrects, running_target_corrects = 0.0, 0.0
-
+                subject_F_loss, subject_D_loss = 0.0, 0.0
+                subject_source_hit, subject_target_hit = 0.0, 0.0
+                subject_source_num, subject_target_num = 0.0, 0.0
                 # traversal all source training dataloader
                 # 这里使用了 enumerate 直接遍历了dataloader中的每个batch
                 if phase == 'train':
+                    feature_extractor.train()
+                    domain_classifier.train()
+                    label_predictor.train()
                     for i, ((source_data, source_label), (target_data, _)) in enumerate(
                             zip(source_train_dataloaders[phase][dataset_index], target_subject_train_loader)):
+
                         source_data = source_data.cuda()  # [256, 1, 8, 52] (target_data 也是这个尺寸)
-                        # print('size of source_data: ', source_data.shape)
-                        # print('size of target_data: ', target_data.shape)
                         source_label = source_label.cuda()
                         target_data = target_data.cuda()
+
                         # 混合 source / target data
                         mixed_data = torch.cat([source_data, target_data], dim=0)  # [512, 1, 8, 52]
                         # print('size of mixed_data:  ', mixed_data.shape)
@@ -260,7 +264,7 @@ for target_subject_train_loader, target_subject_valid_loader in zip(list_target_
                         feature = feature_extractor(mixed_data)  # [512, 64, 4, 4]
                         domain_logits = domain_classifier(feature.detach())
                         loss = domain_criterion(domain_logits, domain_label)
-                        # running_D_loss += loss.item()
+                        subject_D_loss += loss.item()       # 一定要使用.item() 将loss张量转化成float格式存储，不然显存会爆
                         loss.backward()
                         optimizer_D.step()
 
@@ -270,7 +274,7 @@ for target_subject_train_loader, target_subject_valid_loader in zip(list_target_
                         # loss 包括 source data 的 label loss 以及 source data 和 target data 的 domain loss
                         loss = class_criterion(class_logits, source_label) - lamb * domain_criterion(domain_logits,
                                                                                                      domain_label)
-                        # running_F_loss += loss.item()
+                        subject_F_loss += loss.item()
                         loss.backward()
                         optimizer_F.step()
                         optimizer_C.step()
@@ -279,24 +283,21 @@ for target_subject_train_loader, target_subject_valid_loader in zip(list_target_
                         optimizer_F.zero_grad()
                         optimizer_C.zero_grad()
 
-                        loss_over_batches += loss
-                        correct_over_batches += torch.sum(torch.argmax(class_logits, dim=1) == source_label).item()
-                        total_num += source_data.shape[0]
+                        num_batches += i
 
-
-                        # total_hit += torch.sum(torch.argmax(class_logits, dim=1) == source_label).item()
-                        # total_num += source_data.shape[0]
-                        # train_accuracy = total_hit / total_num
-                        #
-                        # print('Training Stage:  Subject:{}  batch:{}    train acc: {:.6f}'
-                        #       .format(dataset_index, i, train_accuracy))
+                        subject_source_hit += torch.sum(torch.argmax(class_logits, dim=1) == source_label).item()
+                        subject_source_num += source_data.shape[0]
+                        # print('train acc: {:.2f}'.format(subject_source_hit / subject_source_num))
 
                 else:
                     label_predictor.eval()
                     feature_extractor.eval()
-                    # print('\nvalidation stage:')
+                    domain_classifier.eval()
+                    # 作 validation 其实就不用 source_target 1:1送进去了， 直接分批全部送进去
+
                     for i, ((source_data, source_label), (target_data, target_label)) in enumerate(
                             zip(source_train_dataloaders[phase][dataset_index], target_subject_valid_loader)):
+
                         source_data = source_data.cuda()  # [256, 1, 8, 52] (target_data 也是这个尺寸)
                         source_label = source_label.cuda()
                         target_data = target_data.cuda()
@@ -319,164 +320,71 @@ for target_subject_train_loader, target_subject_valid_loader in zip(list_target_
                         # calculate loss
                         loss_domain = domain_criterion(domain_target_logits, target_domain_label) \
                                       + domain_criterion(domain_source_logits, source_domain_label)
+                        subject_D_loss += loss_domain.item()
+
                         loss_class = class_criterion(class_source_logits, source_label)
                         loss = loss_class - lamb * loss_domain
+                        subject_F_loss += loss.item()
 
-                        # running_loss += loss
+                        num_batches += i
 
-                        # batch_D_loss += loss_domain
-                        # batch_F_loss += loss
+                        subject_source_hit += torch.sum(torch.argmax(class_source_logits, dim=1) == source_label).item()
+                        subject_source_num += source_data.shape[0]
+                        subject_target_hit += torch.sum(torch.argmax(class_target_logits, dim=1) == target_label).item()
+                        subject_target_num += target_data.shape[0]
 
-                        # source domain
-                        correct_source_over_batches += torch.sum(torch.argmax(class_source_logits, dim=1)
-                                                                 == source_label).item()
-                        total_source_num += source_data.shape[0]
+                        # print('target_batch_valid_acc:{:.4f}'.format(torch.sum(
+                        #     torch.argmax(class_target_logits, dim=1) == target_label).item() / target_data.shape[0]))
 
-                        # target domain
-                        correct_target_over_batches += torch.sum(torch.argmax(class_target_logits, dim=1)
-                                                                 == target_label).item()
-                        total_target_num += target_data.shape[0]
+                        # print('val source acc: {:.2f}'.format(subject_source_hit / subject_source_num))
+                        # print('val target acc: {:.2f}'.format(subject_target_hit / subject_target_num))
 
+                epoch_F_loss += subject_F_loss
+                epoch_D_loss += subject_D_loss
 
-                        # source_total_hit += torch.sum(torch.argmax(class_source_logits, dim=1) == source_label).item()
-                        # source_total_num += source_data.shape[0]
-                        # source_accuracy = source_total_hit / source_total_num
-                        #
-                        # target_total_hit += torch.sum(torch.argmax(class_target_logits, dim=1) == target_label).item()
-                        # target_total_num += target_data.shape[0]
-                        # target_accuracy = target_total_hit / target_total_num
-                        #
-                        # print('{}   Loss:  {:.8f}   s_Acc:  {:.8f}  t_Acc:  {:.8}'
-                        #       .format(phase, loss, source_accuracy, target_accuracy))
+                epoch_source_hit += subject_source_hit
+                epoch_source_num += subject_source_num
+                epoch_target_hit += subject_target_hit
+                epoch_target_num += subject_target_num
 
-                # summarize epoch result
-                if phase == 'train':
-                    running_loss += loss_over_batches
-                    running_corrects += correct_over_batches
-                    running_total_num = total_num
-                    subject_loss = running_loss / running_total_num
-                    subject_acc = running_corrects / running_total_num
+                epoch_F_loss = epoch_F_loss / num_batches
+                epoch_D_loss = epoch_D_loss / num_batches
 
-                    print('{} phase  Subject:{}  Loss:{:.6f}  Acc:{:.4f}'
-                          .format(phase, dataset_index, subject_loss, subject_acc))
+            if phase == 'valid':
+                scheduler_F.step(epoch_F_loss)
+                scheduler_D.step(epoch_D_loss)
+                if epoch_F_loss + precision < best_loss_F:
+                    # print('epoch_F_loss: ', epoch_F_loss)
+                    # print('best_F_loss: ', best_loss_F)
+                    # print()
+                    # print('New best validation F_loss', epoch_F_loss)
 
-                else:
-                    running_F_loss += batch_F_loss
-                    running_D_loss += batch_D_loss
-                    running_source_corrects += correct_source_over_batches
-                    running_target_corrects += correct_target_over_batches
-                    running_total_source_num = total_source_num
-                    running_total_target_num = total_target_num
+                    best_loss_F = epoch_F_loss
 
-                    subject_F_loss = running_F_loss / (total_source_num + total_target_num)
-                    subject_D_loss = running_D_loss / (total_source_num + total_target_num)
-                    subject_source_acc = running_source_corrects / running_total_source_num
-                    subject_target_acc = running_target_corrects / running_total_target_num
+                    best_weights_feature_extractor = copy.deepcopy(feature_extractor.state_dict())
+                    best_weights_domain_classifier = copy.deepcopy(domain_classifier.state_dict())
+                    best_weights_label_predictor = copy.deepcopy(label_predictor.state_dict())
+                    patience = patience_increase + epoch
+                    print('patience: ', patience)
 
-                    print('{} phase Subject:{}  F_Loss:{:.6f}  D_loss:{:.6f}  Source_Acc:{:.4}  Target_Acc:{:.4}'
-                          .format(phase, dataset_index, subject_F_loss, subject_D_loss, subject_source_acc, subject_target_acc))
+            if phase == 'valid':
+                # valid stage results
+                print('{} phase:    D_loss:{:.6f}   F_loss:{:.6f}   Source_acc:{:.6f}   Target_acc:{:.6}'.
+                      format(phase, epoch_D_loss, epoch_F_loss, epoch_source_hit / epoch_source_num,
+                             epoch_target_hit / epoch_target_num))
+            else:
+                # train stage result
+                print('{} phase:    D_loss:{:.6f}   F_loss:{:.6f}   Source_acc:{:.6}'.
+                      format(phase, epoch_D_loss, epoch_F_loss, epoch_source_hit / epoch_source_num))
 
+        print('Epoch {}/{} took {:.3f}s'.format(epoch + 1, num_epochs, time.time() - epoch_start))
 
+        if epoch > patience:
+            break
 
+    single_mode = True
+    if single_mode == True:
+        break
 
-
-
-
-
-
-
-
-                # loss_over_datasets += loss
-                #
-                # running_loss =
-                # loss_over_datasets += loss
-                # correct_over_datasets +=
-
-    target_subject_num += 1
-
-
-
-                #
-                # for i, ((source_data, source_label), (target_data, _)) in enumerate(
-                #         zip(source_train_dataloaders[phase][dataset_index], target_subject_train_loader)):
-                #
-                #     if phase == 'train':
-                #         source_data = source_data.cuda()                # [256, 1, 8, 52] (target_data 也是这个尺寸)
-                #         # print('size of source_data: ', source_data.shape)
-                #         # print('size of target_data: ', target_data.shape)
-                #         source_label = source_label.cuda()
-                #         target_data = target_data.cuda()
-                #         # 混合 source / target data
-                #         mixed_data = torch.cat([source_data, target_data], dim=0)   # [512, 1, 8, 52]
-                #         # print('size of mixed_data:  ', mixed_data.shape)
-                #         domain_label = torch.zeros([source_data.shape[0] + target_data.shape[0], 1]).cuda()
-                #         # 设定 source data 的 label 是 1
-                #         domain_label[: source_data.shape[0]] = 1
-                #
-                #         # train Domain Classifier
-                #         feature = feature_extractor(mixed_data)         # [512, 64, 4, 4]
-                #         # print(feature.shape)
-                #         # time.sleep(1000)
-                #         domain_logits = domain_classifier(feature.detach())
-                #         loss = domain_criterion(domain_logits, domain_label)
-                #         running_D_loss += loss.item()
-                #         loss.backward()
-                #         optimizer_D.step()
-                #
-                #         # train Feature Extractor and Domain Classifier
-                #         class_logits = label_predictor(feature[:source_data.shape[0]])
-                #         domain_logits = domain_classifier(feature)
-                #         loss = class_criterion(class_logits, source_label) - lamb * domain_criterion(domain_logits, domain_label)
-                #         running_F_loss += loss.item()
-                #         loss.backward()
-                #         optimizer_F.step()
-                #         optimizer_C.step()
-                #
-                #         optimizer_D.zero_grad()
-                #         optimizer_F.zero_grad()
-                #         optimizer_C.zero_grad()
-                #
-                #         total_hit += torch.sum(torch.argmax(class_logits, dim=1) == source_label).item()
-                #         total_num += source_data.shape[0]
-                #         train_accuracy = total_hit / total_num
-                #
-                #         print('Training Stage:  Subject:{}  batch:{}    train acc: {:.6f}'
-                #               .format(dataset_index, i, train_accuracy), end='\r')
-                #
-                #     else:
-                #         # evaluation process
-                #         label_predictor.eval()
-                #         feature_extractor.eval()
-                #         print('validation stage:')
-                #         time.sleep(1000)
-
-
-
-
-
-
-
-
-
-
-
-
-                    # print(type(source_data))
-                    # print('source_data: ', source_data)
-                    # print(type(source_label))
-                    # print('source_label: ', source_label)
-                    # print(type(target_data))
-                    # print('target_data: ', target_data)
-                    # time.sleep(1000)
-
-
-
-
-
-
-
-
-
-
-
+# %% Testing Stage
 
